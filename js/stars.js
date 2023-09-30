6c3899c6ba9220b26707e4c7b6524d9a68c323fe
Simbaclaws <h.hellinga@inner-join.nl>
2023-10-01 00:33:22 +0200

    Made some small changes for the design

--- js/stars.js
+++ js/stars.js
@@ -1,43 +1,80 @@
-const canvas = document.getElementById("starfield");
-const ctx = canvas.getContext("2d");
+function Star(x,y,r,color){
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.rChange = 0.015;
+  // this.vx = Math.floor(Math.random()*4+1);
+  // this.vy = Math.floor(Math.random()*4+1);
+  this.color = color;
+}
 
-let stars = []; // This array will hold your stars
-const numStars = 200; // Number of stars to display
+Star.prototype = {
+  constructor: Star,
+  render: function(){
+    context.beginPath();
+    context.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
+    context.shadowBlur = 8;
+    context.shadowColor = "white";
+    context.fillStyle = this.color;
+    context.fill();
+  },
+  update: function(){
+
+    if (this.r > 2 || this.r < .8){
+      this.rChange = - this.rChange;
+    }
+    this.r += this.rChange;
+  }
+}
+
+const canvas = document.getElementById("starfield");
+const context = canvas.getContext("2d");
+
+const canvasWidth = canvas.width = document.body.offsetWidth;
+const canvasHeight = canvas.height = document.body.offsetHeight;
+const centerX = canvasWidth / 2;
+const centerY = canvasHeight / 2;
+
+const numStars = 200;
 let speed = 0;
 let reverse = false;
 
-// Set canvas size to full window
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
-
-// Function to create a star object with random properties
-function createStar() {
-  const x = Math.random() * canvas.width;
-  const y = Math.random() * canvas.height;
-  const radius = Math.random() * 3;
-  return { x, y, radius };
+function randomColor(){
+  const arrColors = ["ffffff", "ffecd3" , "bfcfff"];
+  return "#"+arrColors[Math.floor((Math.random()*3))];
 }
 
-// Populate the stars array
-for (let i = 0; i < numStars; i++) {
-  stars.push(createStar());
+let arrStars = [];
+for(let i = 0; i < numStars; i++){
+  let randX = Math.floor((Math.random()*canvasWidth)+1);
+  let randY = Math.floor((Math.random()*canvasHeight)+1);
+  let randR = Math.random() * 1.7 + .5;
+
+  let star = new Star(randX, randY, randR, randomColor());
+  arrStars.push(star);
 }
-
-function drawStars() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-
-  stars.forEach((star) => {
-    ctx.beginPath();
-    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2, false);
-    ctx.fillStyle = "#ffffff"; // White color for the stars
-    ctx.fill();
-  });
+function update(){
+  for(let i = 0; i < arrStars.length; i ++){
+    arrStars[i].update();
+  }
 }
-
+function animate(){
+  update();
+  /*
+    Remove comments below these for a cool trailing effect & comment
+    out the context.clearRect.
+  */
+  context.fillStyle = 'rgba(255, 255, 255, .1)';
+  context.fillRect(0,0, canvasWidth, canvasHeight);
+  context.clearRect(0,0, canvasWidth, canvasHeight);
+  for(let i = 0; i < arrStars.length; i++){
+    arrStars[i].render();
+  }
+  requestAnimationFrame(animate);
+}
 function updateStars() {
-  stars.forEach((star) => {
+  arrStars.forEach((star) => {
+    console.log(star);
     // Calculate the direction from the star to the center
     let dirX = centerX - star.x;
     let dirY = centerY - star.y;
@@ -56,39 +93,34 @@ function updateStars() {
       // If the star goes off the canvas, reset its position to the center
       if (
         star.x < 0 ||
-        star.x > canvas.width ||
+        star.x > canvasWidth ||
         star.y < 0 ||
-        star.y > canvas.height
+        star.y > canvasHeight
       ) {
-        star.x = Math.random() * canvas.width;
-        star.y = Math.random() * canvas.height;
+        star.x = Math.random() * canvasWidth;
+        star.y = Math.random() * canvasHeight;
       }
     } else {
       star.x += dirX * speed;
       star.y += dirY * speed;
       // If the star reaches the center, reset its position
       if (distance < 1) {
-        star.x = Math.random() * canvas.width;
-        star.y = Math.random() * canvas.height;
+        star.x = Math.random() * canvasWidth;
+        star.y = Math.random() * canvasHeight;
       }
     }
   });
 }
 
+animate();
 function animateStars() {
   updateStars();
-  drawStars();
   requestAnimationFrame(() => animateStars()); // Save the ID of the animation frame
 }
-
 function playAnimationFor(duration) {
-  speed = 4;
+  speed = 2;
+  animateStars();
   setTimeout(() => {
     speed = 0;
   }, duration);
 }
-
-// Initial draw
-drawStars();
-
-animateStars(false);
